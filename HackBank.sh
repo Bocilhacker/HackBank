6e289ce4cd3c96a521abdfbffe9eb63f83be53d1
Bocilhacker <101480437+Bocilhacker@users.noreply.github.com>
2022-03-13 14:34:36 +0700

    Add files via upload

---
+++ HackBank.sh
@@ -0,0 +1,41 @@
+GNU nano 6.2                          HackBank.sh
+#!/bin/sh
+
+sleep 1
+   echo "SUBCRIBE DULU TOD.." | lolcat
+      sleep 3
+          xdg-open https://youtube.com/channel/UC5msFOy-RulyPmiF8ka1t8A
+
+sleep 3
+clear
+
+sleep 10
+# tampilan
+toilet -f big -F gay Hack Bank
+echo
+echo "================================================================"
+echo " [●] Author : Mr_Sadboy01       [●]"
+echo " [●] Team   : CYBER DARK SISTEM [●]"
+echo " [●] Whatsap: 083197689414      [●]"
+echo "================================================================"
+echo
+echo "[●] JANGAN DI SALAH GUNAKAN [●] "
+echo "================================================================"
+echo
+echo "MASUKKAN  NAMA BANK YANG MAU LU RETAS CONTOH (BRI.BCA.BNI)" | lolcat
+read -p "$ " bank
+echo
+sleep 3
+echo
+echo "MASUKKAN NOMOR REKENING LU BOSS" | lolcat
+read -p "$  " norek
+echo
+sleep 3
+echo
+echo "MASUKKAN MINIMAL UANG YANG  INGIN KAU HACK" | lolcat
+read -p "$   " Masuk
+sleep 3
+echo
+echo $bank nomor $norek Telah Di Transfer Sebesar $Masuk
+echo
+echo "#JANGAN SALAHKAN ADDMIN JIKA  ANDI DITANGGKA POLISI" | lolcat00
